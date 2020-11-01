935132b6838c7bcedd07543cd09d9576b406d99d
Valeriy Kuzmin <malcoriel@gmail.com>
2020-11-01 14:28:25 +0100

    test: bunch of failing tests

--- src/tests/smoke.spec.ts
+++ src/tests/smoke.spec.ts
@@ -1,5 +1,55 @@
+/*
+*
+    creating and updating users
+    creating and updating a users favourite subredits
+    setting the newsletter send out time for each user (default: 8am)
+    Turning on and off the newsletter send out for a specific user
+    triggering the send of a newsletter to each respective users email at
+    each users specified send out time (more on this in the section: "Our part")
+* */
+
 describe("reddit-notifier", () => {
-    it("can run a test", () => {
-        expect(true).toBe(false);
-    })
-})
+  it("can create a user", () => {
+    expect(true).toBe(false);
+  });
+
+  it("can update a user", () => {
+    expect(true).toBe(false);
+  });
+
+  it("extra: can list users", () => {
+    expect(true).toBe(false);
+  });
+
+  it("extra: can delete a user", () => {
+    expect(true).toBe(false);
+  });
+
+  it("can create/set a list of favorite subreddits for a user", () => {
+    expect(true).toBe(false);
+  });
+
+  it("can set the email send time", () => {
+    expect(true).toBe(false);
+  });
+
+  it("can turn on the email for a user", () => {
+    expect(true).toBe(false);
+  });
+
+  it("can turn off the email for a user", () => {
+    expect(true).toBe(false);
+  });
+
+  it("can trigger a news email at the specified time", () => {
+    expect(true).toBe(false);
+  });
+
+  it("can list subreddits", () => {
+    expect(true).toBe(false);
+  });
+
+  it("can get last 3 most-voted posts from a subreddit", () => {
+    expect(true).toBe(false);
+  });
+});
