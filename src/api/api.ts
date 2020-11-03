9972974c1bd86ce3e00fd518d5c472a1ee67f9e5
Valeriy Kuzmin <malcoriel@gmail.com>
2020-11-03 11:15:48 +0100

    feat: add swagger-ui

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,15 +4,18 @@ import logger from "../logging/logging";
 import * as locator from "../locator/locator";
 import { usersRouter } from "./usersRouter";
 import { subscriptionsRouter } from "./subscriptionsRouter";
+import swaggerUi from "swagger-ui-express";
+import yaml from "yamljs";
+const swaggerDocument = yaml.load("./src/swagger/swagger.yaml");
 
 const app = express();
-
 const apiConfig = config.getTyped("root").api;
 
 locator.getScheduler({ singleton: true });
 
 app.use("/users", usersRouter);
 app.use("/subscriptions", subscriptionsRouter);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(apiConfig.port, () => {
   logger.info(`API handlers are listening on port ${apiConfig.port}`);
