149f78f8c927d959cdb9efae8249fb1b194fc42c
Valeriy Kuzmin <malcoriel@gmail.com>
2020-11-03 11:25:27 +0100

    fix: body-parser

--- src/api/api.ts
+++ src/api/api.ts
@@ -6,15 +6,17 @@ import { usersRouter } from "./usersRouter";
 import { subscriptionsRouter } from "./subscriptionsRouter";
 import swaggerUi from "swagger-ui-express";
 import yaml from "yamljs";
+import bodyParser from "body-parser";
 const swaggerDocument = yaml.load("./src/swagger/swagger.yaml");
 
 const app = express();
 const apiConfig = config.getTyped("root").api;
+app.use(bodyParser.json());
 
 locator.getScheduler({ singleton: true });
 
-app.use("/users", usersRouter);
-app.use("/subscriptions", subscriptionsRouter);
+app.use("/user", usersRouter);
+app.use("/subscription", subscriptionsRouter);
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(apiConfig.port, () => {
